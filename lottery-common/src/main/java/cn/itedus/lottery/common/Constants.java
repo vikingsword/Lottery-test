8562527f3e845e0608b50c935bb73c1c69b21d91
vikingar <423141759@qq.com>
2024-03-19 18:54:44 +0800

    test

--- lottery-common/src/main/java/cn/itedus/lottery/common/Constants.java
+++ lottery-common/src/main/java/cn/itedus/lottery/common/Constants.java
@@ -59,4 +59,72 @@ public class Constants {
             return info;
         }
     }
+
+    public enum AwardState {
+        /**
+         * 等待发奖
+         */
+        WAIT(0, "等待发奖"),
+
+        /**
+         * 发奖成功
+         */
+        SUCCESS(1, "发奖成功"),
+
+        /**
+         * 发奖失败
+         */
+        FAILURE(2, "发奖失败");
+
+        private Integer code;
+        private String info;
+
+        AwardState(Integer code, String info) {
+            this.code = code;
+            this.info = info;
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public String getInfo() {
+            return info;
+        }
+    }
+
+    public enum AwardType {
+        /**
+         * 文字描述
+         */
+        DESC(1, "文字描述"),
+        /**
+         * 兑换码
+         */
+        RedeemCodeGoods(2, "兑换码"),
+        /**
+         * 优惠券
+         */
+        CouponGoods(3, "优惠券"),
+        /**
+         * 实物奖品
+         */
+        PhysicalGoods(4, "实物奖品");
+
+        private Integer code;
+        private String info;
+
+        AwardType(Integer code, String info) {
+            this.code = code;
+            this.info = info;
+        }
+
+        public Integer getCode() {
+            return code;
+        }
+
+        public String getInfo() {
+            return info;
+        }
+    }
 }
